82bb7eed1b92b7ff74dbba1bef95e9c5c081dbb3
adamhaze8 <44332188+adamhaze8@users.noreply.github.com>
2024-11-13 10:12:37 -0500

    Fixed up some comments

--- Python/PID_Controller.py
+++ Python/PID_Controller.py
@@ -1,31 +1,24 @@
 class PID_Controller:
     def __init__(self, kp, ki, kd, setpoint=0):
-        self.kp = kp  # Proportional gain
-        self.ki = ki  # Integral gain
-        self.kd = kd  # Derivative gain
-        self.setpoint = setpoint  # Desired target value
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
 
         self.prev_error = 0  # Store previous error for derivative term
         self.integral = 0  # Store accumulated integral
 
     def update(self, current_value, dt):
-        # Calculate error
         error = self.setpoint - current_value
-
-        # Proportional term
         P = self.kp * error
 
-        # Integral term
         self.integral += error * dt
         I = self.ki * self.integral
 
-        # Derivative term
         derivative = (error - self.prev_error) / dt
         D = self.kd * derivative
 
-        # Update previous error
         self.prev_error = error
 
-        # PID output
         output = P + I + D
         return output
